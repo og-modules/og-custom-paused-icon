10250dcfd03edb639a97b4d4c283244dd9c94a64
Carl-Hugo Marcotte <carlhugo@gmail.com>
2025-09-05 00:48:30 -0400

    debug: add "module loading..." message

--- index.ts
+++ index.ts
@@ -1,22 +1,27 @@
-import ModuleInfo from './module.json' assert { type: 'json' };
+import ModuleInfo from "./module.json" assert { type: "json" };
+
+console.log("og-custom-paused-icon: module loading...");
 
 //og-custom-paused-icon
 export class CustomPausedIcon extends og.BaseModule {
     public override initialize(): void {
-        this.hooks.on('renderPause', (pauseLayer: any, html: any, data: any) => {
-            this.updateIcon(html[0]);
-        });
-        this.hooks.on('ready', () => {
+        this.hooks.on(
+            "renderPause",
+            (pauseLayer: any, html: any, data: any) => {
+                this.updateIcon(html[0]);
+            }
+        );
+        this.hooks.on("ready", () => {
             this.updateIcon(document);
         });
     }
 
     updateIcon(el: any) {
-        const pauseIcon = el.querySelector('#paused img');
+        const pauseIcon = el.querySelector("#paused img");
         if (pauseIcon) {
             //@ts-ignore
             pauseIcon.src = `modules/${this.id}/og-paused-icon-128x128.webp`;
-            this.logDebug('Updated paused icons');
+            this.logDebug("Updated paused icons");
         }
     }
 }
