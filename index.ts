25f56bcdb28723c54be0b5dbccf09e84c7091570
Carl-Hugo Marcotte <carlhugo@gmail.com>
2025-09-05 01:43:41 -0400

    Covnert hooks to normal FVTT

--- index.ts
+++ index.ts
@@ -1,32 +1,46 @@
 import ModuleInfo from "./module.json" assert { type: "json" };
 
-console.log("og-custom-paused-icon: module loading...", ModuleInfo);
+console.debug("og-custom-paused-icon: module loading...", ModuleInfo);
 
 //og-custom-paused-icon
 export class CustomPausedIcon extends og.BaseModule {
     public override initialize(): void {
-        this.hooks.on(
+        console.debug("og-custom-paused-icon: initializing...");
+
+        var me = this;
+        Hooks.once("ready", async function () {
+            console.debug("og-custom-paused-icon: hooks-ready");
+            me.updateIcon(document);
+        });
+        Hooks.on(
             "renderPause",
-            (pauseLayer: any, html: any, data: any) => {
-                console.log("og-custom-paused-icon: hooks-renderPause");
-                this.updateIcon(html[0]);
+            async function (pauseLayer: any, html: any, data: any) {
+                console.debug("og-custom-paused-icon: hooks-renderPause");
+                me.updateIcon(html[0]);
             }
         );
-        this.hooks.on("ready", () => {
-            console.log("og-custom-paused-icon: hooks-ready");
-            this.updateIcon(document);
-        });
-        console.log("og-custom-paused-icon: initialized");
+        // this.hooks.on(
+        //     "renderPause",
+        //     (pauseLayer: any, html: any, data: any) => {
+        //         console.debug("og-custom-paused-icon: hooks-renderPause");
+        //         this.updateIcon(html[0]);
+        //     }
+        // );
+        // this.hooks.on("ready", () => {
+        //     console.debug("og-custom-paused-icon: hooks-ready");
+        //     this.updateIcon(document);
+        // });
+        console.debug("og-custom-paused-icon: initialized");
     }
 
     updateIcon(el: any) {
-        // const pauseIcon = el.querySelector("#paused img");
-        // if (pauseIcon) {
-        //     //@ts-ignore
-        //     pauseIcon.src = `modules/${this.id}/og-paused-icon-128x128.webp`;
-        //     this.logDebug("Updated paused icons");
-        // }
-        console.log("og-custom-paused-icon: updateIcon");
+        console.debug("og-custom-paused-icon: updateIcon");
+        const pauseIcon = el.querySelector("#paused img");
+        if (pauseIcon) {
+            //@ts-ignore
+            pauseIcon.src = `modules/${this.id}/og-paused-icon-128x128.webp`;
+            this.logDebug("Updated paused icons");
+        }
     }
 }
 
