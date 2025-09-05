a14c4b2230e6cc4d5d9f81a27e12bffcce7be82f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2025-09-05 00:58:44 -0400

    keep only a console log

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
-import ModuleInfo from "./module.json" assert { type: "json" };
+// import ModuleInfo from "./module.json" assert { type: "json" };
 
 console.log("og-custom-paused-icon: module loading...");
 
-//og-custom-paused-icon
-export class CustomPausedIcon extends og.BaseModule {
-    public override initialize(): void {
-        this.hooks.on(
-            "renderPause",
-            (pauseLayer: any, html: any, data: any) => {
-                this.updateIcon(html[0]);
-            }
-        );
-        this.hooks.on("ready", () => {
-            this.updateIcon(document);
-        });
-    }
+// //og-custom-paused-icon
+// export class CustomPausedIcon extends og.BaseModule {
+//     public override initialize(): void {
+//         this.hooks.on(
+//             "renderPause",
+//             (pauseLayer: any, html: any, data: any) => {
+//                 this.updateIcon(html[0]);
+//             }
+//         );
+//         this.hooks.on("ready", () => {
+//             this.updateIcon(document);
+//         });
+//     }
 
-    updateIcon(el: any) {
-        const pauseIcon = el.querySelector("#paused img");
-        if (pauseIcon) {
-            //@ts-ignore
-            pauseIcon.src = `modules/${this.id}/og-paused-icon-128x128.webp`;
-            this.logDebug("Updated paused icons");
-        }
-    }
-}
+//     updateIcon(el: any) {
+//         const pauseIcon = el.querySelector("#paused img");
+//         if (pauseIcon) {
+//             //@ts-ignore
+//             pauseIcon.src = `modules/${this.id}/og-paused-icon-128x128.webp`;
+//             this.logDebug("Updated paused icons");
+//         }
+//     }
+// }
 
-// Create an instance of the module and register it with Og Core Library.
-og.registerModule(() => new CustomPausedIcon(ModuleInfo));
+// // Create an instance of the module and register it with Og Core Library.
+// og.registerModule(() => new CustomPausedIcon(ModuleInfo));
